f675b873d92274dce6005393cca45b41e40c2203
caioocardoso <caiooliveiracardosoo@gmail.com>
2024-12-08 18:40:33 -0300

    questao 2 funcionando

--- RL0Q2.c
+++ RL0Q2.c
@@ -1,21 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <math.h>
 #include <stdbool.h>
+#include <ctype.h>
 
-typedef struct points
+typedef struct point
 {
     int ordem_entrada;
     float distancia_origem;
     float x;
     float y;
-} points;
+} point;
 
 typedef struct nomes
 {
     char nome[100];
 } nomes;
+
+void ordenador(nomes nameStr[], int nameCont, int numbersInt[], int intCont, float numbersFloat[], int floatCont, point numbersPoints[], int pointCont)
+{
+    for (int i = 0; i < nameCont; i++)
+    {
+        for (int j = 0; j < nameCont - 1 - i; j++)
+        {
+            nomes aux;
+            if (strcmp(nameStr[j].nome, nameStr[j + 1].nome) > 0)
+            {
+                aux = nameStr[j];
+                nameStr[j] = nameStr[j + 1];
+                nameStr[j + 1] = aux;
+            }
+        }
+    }
+    for (int i = 0; i < intCont; i++)
+    {
+        for (int j = 0; j < intCont - 1 - i; j++)
+        {
+            int aux;
+            if (numbersInt[j] > numbersInt[j + 1])
+            {
+                aux = numbersInt[j];
+                numbersInt[j] = numbersInt[j + 1];
+                numbersInt[j + 1] = aux;
+            }
+        }
+    }
+    for (int i = 0; i < floatCont; i++)
+    {
+        for (int j = 0; j < floatCont - 1 - i; j++)
+        {
+            float aux;
+            if (numbersFloat[j] > numbersFloat[j + 1])
+            {
+                aux = numbersFloat[j];
+                numbersFloat[j] = numbersFloat[j + 1];
+                numbersFloat[j + 1] = aux;
+            }
+        }
+    }
+    for (int i = 0; i < pointCont; i++)
+    {
+        for (int j = 0; j < pointCont - 1 - i; j++)
+        {
+            point aux;
+            if (numbersPoints[j].distancia_origem > numbersPoints[j + 1].distancia_origem)
+            {
+                aux = numbersPoints[j];
+                numbersPoints[j] = numbersPoints[j + 1];
+                numbersPoints[j + 1] = aux;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -28,35 +85,48 @@ int main()
         printf("Os arquivos nao puderam ser abertos!");
         return EXIT_FAILURE;
     }
-    int lineCount = 0;
+    int linha = 0;
 
     while (fgets(str, sizeof(str), fp_in) != NULL)
     {
         float numbersFloat[100];
-        int isFloat = false;
         int numbersInt[100];
+        point numbersPoints[100];
         nomes nameStr[100];
+        int isFloat = false;
 
-        int floatCont = 0, intCont = 0, nameCont = 0;
+        int floatCont = 0, intCont = 0, nameCont = 0, pointCont = 0;
 
         char *token = strtok(str, " ");
 
         while (token != NULL)
         {
             if (isdigit(token[0]) || token[0] == '-')
-            { // integer or float
+            {
+                isFloat = false;
                 for (int i = 0; i < strlen(token); i++)
-                    if (token[i] == ".")
+                {
+                    if (token[i] == '.')
+                    {
                         isFloat = true;
+                        break;
+                    }
+                }
                 if (isFloat)
                     numbersFloat[floatCont++] = atof(token);
                 else
                     numbersInt[intCont++] = atoi(token);
             }
-            else if (token[0] == "(")
+            else if (token[0] == '(')
             {
                 float x, y;
-                sscanf(token, "(%f,%f)", &x, &y);
+                if (sscanf(token, "(%f,%f)", &x, &y) == 2)
+                {
+                    numbersPoints[pointCont].x = x;
+                    numbersPoints[pointCont].y = y;
+                    numbersPoints[pointCont].ordem_entrada = pointCont;
+                    numbersPoints[pointCont++].distancia_origem = sqrt(x * x + y * y);
+                }
             }
             else
             {
@@ -64,9 +134,50 @@ int main()
             }
             token = strtok(NULL, " ");
         }
-        return 0;
-    }
+        // Ordenação
+        ordenador(nameStr, nameCont, numbersInt, intCont, numbersFloat, floatCont, numbersPoints, pointCont);
+        char text[1000];
+        if(linha==0)sprintf(text, "str:" );
+        else sprintf(text, "\nstr:");
+        for (int i = 0; i < nameCont; i++)
+        {
+            if (i == 0)
+                sprintf(text, "%s%s", text, nameStr[i].nome);
+            else
+                sprintf(text, "%s %s", text, nameStr[i].nome);
+        }
+        sprintf(text, "%s %s", text, "int:");
+        for (int i = 0; i < intCont; i++)
+        {
+            if (i == 0)
+                sprintf(text, "%s%d", text, numbersInt[i]);
+            else
+                sprintf(text, "%s %d", text, numbersInt[i]);
+        }
 
-    // Ordenação
-    ordenarString(nameStr, nameCont);
+        sprintf(text, "%s %s", text, "float:");
+        for (int i = 0; i < floatCont; i++)
+        {
+            if (i == 0)
+                sprintf(text, "%s%.5g", text, numbersFloat[i]);
+            else
+                sprintf(text, "%s %.5g", text, numbersFloat[i]);
+        }
+
+        sprintf(text, "%s %s", text, "p:");
+        for (int i = 0; i < pointCont; i++)
+        {
+            if (i == 0)
+                sprintf(text, "%s(%.5g,%.5g)", text, numbersPoints[i].x, numbersPoints[i].y);
+            else
+                sprintf(text, "%s (%.5g,%.5g)", text, numbersPoints[i].x, numbersPoints[i].y);
+        }
+
+        fputs(text, fp_out);
+        linha++;
+    }
+    
+    fclose(fp_in);
+    fclose(fp_out);
+    return EXIT_SUCCESS;
 }
